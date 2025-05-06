e88b816de130656a609993c5ff22cfe419ede3ba
zakibn18 <jekben815@gmail.com>
2025-05-06 10:43:42 +0700

    merubah struct menjadi oop

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -1,22 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// membuat stuct node
-struct node
-{
-  int noMhs;
-  node *next;
-};
-
-// set value START = NULL
-node *start = NULL;
-
-// membuat prosedur addNode()
-void addNode()
-{
-  
-}
-
 int main()
 {
   
