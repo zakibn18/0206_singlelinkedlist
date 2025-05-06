2f783d6fcd6a7076e3dd04f321d5721d2605b6c5
zakibn18 <jekben815@gmail.com>
2025-05-06 11:26:50 +0700

    membuat prosedur traverse

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -60,6 +60,53 @@ class linkedList
     {
       return (START == NULL);
     }
+    bool Shearch(int nim, node **previos, node **current)
+    {
+      *previos = START;
+      *current = START;
+
+      while ((*current != NULL) && (nim != (*current)->noMhs))
+      {
+        *previos = *current;
+        *current = (*current)->next;
+      }
+
+      return(*current != NULL);
+    }
+
+    bool delNode(int nim)
+    {
+      node *current, *previous;
+      if (!Shearch(nim, &previous, & current))
+        return false;
+
+      if (current == START)
+        START = START->next;
+      else
+        previous->next = current ->next;
+
+      delete current;
+      return true;
+    }
+
+    void traverse()
+    {
+      if (listEmpty())
+      {
+        cout << "\nList Kosong\n";
+      }
+      else
+      {
+        cout << "\nData didalam list adalah : \n";
+        node *currentNode = START;
+        while (currentNode != NULL)
+        {
+          cout << currentNode-> noMhs << endl;
+          currentNode = currentNode->next;
+        }
+        cout << endl;
+      }
+    }
 };
 
 int main()
